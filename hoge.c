05f2937cccb8e00127f37cdc9bc6154e6be2adb5
n4cr4 <itosukenao@gmail.com>
2022-12-28 22:23:14 +0900

    first commit

---
+++ hoge.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#define hoge 1000
+int main(){
+   //hogeを出力する
+   puts(hoge);
+   /* this is hogeを出力する*/
+   printf("this is hoge");
+}
---
+++ sample.sh
@@ -0,0 +1,6 @@
+$> cd univTask
+$> ls
+codeblock.pdf  codeblock.tex  figure.tex  hoge.c  sample.sh  sample.tex  table.tex
+$> foo="hoge"
+$> echo $foo
+hoge
